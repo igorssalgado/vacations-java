1f326a99661292d6ebdb2867bf42cc99a328fc77
igorssalgado <igorssalgado@outlook.com>
2024-06-07 15:17:00 -0300

    fix: inserted RequestParam import

--- src/main/java/com/vacationsapp/vacations/controllers/VacationController.java
+++ src/main/java/com/vacationsapp/vacations/controllers/VacationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import com.vacationsapp.vacations.models.VacationDTO;
 import com.vacationsapp.vacations.models.VacationModel;
 import com.vacationsapp.vacations.services.VacationService;
